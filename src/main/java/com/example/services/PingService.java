622d82efce3710ac01c5301d67e544a78fddae66
thebrettd <thebrettd@gmail.com>
2014-07-20 23:33:35 -0700

    Ping service

---
+++ src/main/java/com/example/services/PingService.java
@@ -0,0 +1,20 @@
+package com.example.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("/ping")
+public class PingService {
+
+
+
+    @GET
+    public Response get() {
+
+        return Response.ok().build();
+    }
+
+
+}
+
